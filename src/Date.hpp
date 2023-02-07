6133d788b713fbec94d89546cf065b058f176179
LIANG X <liangx0111@outlook.com>
2023-02-07 14:20:47 -0500

    remove incomplete files and format new files to fit in existing code

--- src/Date.hpp
+++ src/Date.hpp
@@ -1,16 +1,18 @@
-#pragma once
-#include <string>
+#ifndef SIMU_DATE_HPP
+#define SIMU_DATE_HPP
+
 #include <iostream>
-//
-//
-//
-//
+#include <string>
+
+namespace simu {
+
 class Date {
- public: Date() = default;
+ public:
+  Date() = default;
   Date(int yyyy, int mm, int dd);
-  Date(const std::string & date);
+  Date(const std::string &date);
   // copy constructor
-  Date(const Date &date) : m_days(date.m_days){};
+  Date(const Date &date) = default;
   // number of days between two dates as a double
   friend double operator-(Date lhs, Date rhs);
   // increase date by <days> number of days
@@ -20,7 +22,13 @@ class Date {
   friend bool operator!=(Date lhs, Date rhs);
   friend bool operator<(Date lhs, Date rhs);
   // read and write from stream
-  friend std::ostream &operator<<(std::ostream &os,Date date);
-  friend std::istream &operator>>(std::istream &os,Date &date);
+  friend std::ostream &operator<<(std::ostream &os, Date date);
+  friend std::istream &operator>>(std::istream &os, Date &date);
+
  private:
-  double m_days; };
+  double m_days;
+};
+
+}  // namespace simu
+
+#endif
